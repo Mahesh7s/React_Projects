ba661eeebca7ddad07cbf88a491d91e0935af256
Mahesh Moka <mokamahesh77@gmail.com>
2025-05-25 11:40:46 +0530

    commiting the autocorret app

---
+++ Autocorrectapp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Autocorrect from './Components/AutoCorrectapp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Autocorrect/>
+    </>
+  )
+}
+
+export default App
