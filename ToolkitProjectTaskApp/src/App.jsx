ba4110aa662ffa9c73414636d9f8ba53f8d6e3cf
Mahesh Moka <mokamahesh77@gmail.com>
2025-06-07 19:55:51 +0530

    commiting the code

---
+++ ToolkitProjectTaskApp/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import {Routes,Route} from "react-router-dom"
+import Navbar from './Pages/Navbar'
+import Admin from './Pages/AdminDashboard'
+import Userb from './Pages/UserDashboard'
+import ViewAllProjects from './Components/ViewAllProjects'
+import CreateProject from './Components/CreateProject'
+import SingleProject from './Components/Project'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element ={<Register/>}/>
+      <Route path="/admin" element={<Admin/>}/>
+      <Route path="/user" element={<Userb/>}/>
+      <Route path="/project/:id" element={<SingleProject/>}/>
+      <Route path="/addProject" element={<CreateProject/>}/>
+      <Route path="/allProjects" element={<ViewAllProjects/>}/>
+
+    </Routes>
+      
+    </>
+  )
+}
+
+export default App
